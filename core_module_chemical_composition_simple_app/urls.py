97629d7ee5e99b956292ece20076e9569ccb502f
Philippe Dessauw <philippe.dessauw@nist.gov>
2020-05-27 09:54:06 -0400

    chore: apply black formatting
    Change-Id: I3817eb216752bbdc63636d8bec770959da96ba81

--- core_module_chemical_composition_simple_app/urls.py
+++ core_module_chemical_composition_simple_app/urls.py
@@ -3,10 +3,14 @@
 
 from django.urls import re_path
 
-from core_module_chemical_composition_simple_app.views import ChemicalCompositionSimpleModule
+from core_module_chemical_composition_simple_app.views import (
+    ChemicalCompositionSimpleModule,
+)
 
 urlpatterns = [
-    re_path(r'module-chemical-composition-simple',
-            ChemicalCompositionSimpleModule.as_view(),
-            name='core_module_chemical_composition_simple'),
+    re_path(
+        r"module-chemical-composition-simple",
+        ChemicalCompositionSimpleModule.as_view(),
+        name="core_module_chemical_composition_simple",
+    ),
 ]
--- core_module_chemical_composition_simple_app/views.py
+++ core_module_chemical_composition_simple_app/views.py
@@ -2,19 +2,29 @@
 """
 import json
 
-from core_module_chemical_composition_app.api import render_chemical_composition, get_chemical_composition_popup_content
+from core_module_chemical_composition_app.api import (
+    render_chemical_composition,
+    get_chemical_composition_popup_content,
+)
 from core_parser_app.tools.modules.views.builtin.popup_module import AbstractPopupModule
 
 
 class ChemicalCompositionSimpleModule(AbstractPopupModule):
     def __init__(self):
-        AbstractPopupModule.__init__(self, button_label='Select Elements',
-                                     styles=['core_module_periodic_table_app/css/periodic.css',
-                                             'core_module_chemical_composition_app/css/'
-                                             'chemical_element_composition.css'],
-                                     scripts=['core_module_chemical_composition_app/js/events.js',
-                                              'core_module_chemical_composition_simple_app/js/'
-                                              'chemical_element_composition_simple.js'])
+        AbstractPopupModule.__init__(
+            self,
+            button_label="Select Elements",
+            styles=[
+                "core_module_periodic_table_app/css/periodic.css",
+                "core_module_chemical_composition_app/css/"
+                "chemical_element_composition.css",
+            ],
+            scripts=[
+                "core_module_chemical_composition_app/js/events.js",
+                "core_module_chemical_composition_simple_app/js/"
+                "chemical_element_composition_simple.js",
+            ],
+        )
 
     def _retrieve_data(self, request):
         """ Retrieve module's data
@@ -25,22 +35,26 @@ class ChemicalCompositionSimpleModule(AbstractPopupModule):
         Returns:
 
         """
-        data = ''
-        if request.method == 'GET':
-            if 'data' in request.GET:
-                data = request.GET['data']
-        elif request.method == 'POST':
-            if 'elementList' in request.POST:
-                element_list = json.loads(request.POST['elementList'])
+        data = ""
+        if request.method == "GET":
+            if "data" in request.GET:
+                data = request.GET["data"]
+        elif request.method == "POST":
+            if "elementList" in request.POST:
+                element_list = json.loads(request.POST["elementList"])
                 if len(element_list) > 0:
                     element_list_xml = ""
                     for element in element_list:
-                        element_list_xml += '<constituent>'
-                        element_list_xml += "<element>" + element['name'] + "</element>"
-                        element_list_xml += "<quantity>" + element['qty'] + "</quantity>"
-                        if element['pur'] != '':
-                            element_list_xml += "<purity>" + element['pur'] + "</purity>"
-                        element_list_xml += '</constituent>'
+                        element_list_xml += "<constituent>"
+                        element_list_xml += "<element>" + element["name"] + "</element>"
+                        element_list_xml += (
+                            "<quantity>" + element["qty"] + "</quantity>"
+                        )
+                        if element["pur"] != "":
+                            element_list_xml += (
+                                "<purity>" + element["pur"] + "</purity>"
+                            )
+                        element_list_xml += "</constituent>"
                     # set the data
                     data = element_list_xml
         return data
@@ -54,14 +68,22 @@ class ChemicalCompositionSimpleModule(AbstractPopupModule):
         Returns:
 
         """
-        return render_chemical_composition(self.data, True, False,
-                                           'core_module_chemical_composition_app/render_data.html')
+        return render_chemical_composition(
+            self.data,
+            True,
+            False,
+            "core_module_chemical_composition_app/render_data.html",
+        )
 
     def _get_popup_content(self):
         """ Return module's data rendering
         """
         # rendering data in the edit form
-        data_template = render_chemical_composition(self.data, True, False,
-                                                    'core_module_chemical_composition_app/edit_data.html')
+        data_template = render_chemical_composition(
+            self.data,
+            True,
+            False,
+            "core_module_chemical_composition_app/edit_data.html",
+        )
 
         return get_chemical_composition_popup_content(self.data, data_template)
