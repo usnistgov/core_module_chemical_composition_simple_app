7aa11140f5cec1cbf28bedb1fa293ec1d42c38bf
Guillaume Sousa <guillaume.sousa@nist.gov>
2022-02-15 18:21:54 +0000

    chore: upgrade 2.0.0-beta1
    Change-Id: I7d8ee0808c644d7192496741ccaa73b1dd90193c

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_module_chemical_composition_simple_app",
-    version="2.0.0-alpha1",
+    version="2.0.0-beta1",
     description="Chemical composition simple module for the parser core project",
     long_description=long_desc,
     author="NIST IT Lab",
